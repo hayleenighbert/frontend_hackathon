f2c127693d3703c779e843e01ab90b625be31bfd
Haylee Nighbert <haylee.nigh@gmail.com>
2015-11-16 00:28:36 -0800

    Styling changes

--- js/logoanimation.js
+++ js/logoanimation.js
@@ -1,12 +1,13 @@
-$(".two")
-  .delay(500)
-  .velocity({ fill: "#333", strokeWidth: 2})
-  .velocity({rotateZ: "10deg"})
-  .delay(500)
-  .velocity({x: "+=20", y: "100%" });
   
 $(".one")
   .delay(2500)
+  .velocity({rotateZ: "10deg"})
+  .delay(500)
+  .velocity({x: "+=20", y: "100%" });
+
+$(".two")
+  .delay(500)
+  .velocity({ fill: "#333", strokeWidth: 2})
   .velocity({rotateZ: "10deg"})
   .delay(500)
   .velocity({x: "+=20", y: "100%" });
