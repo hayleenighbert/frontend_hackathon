c57e328bd2dd4ae3aa5afe5c0feeb3c1af8e7689
Haylee Nighbert <haylee.nigh@gmail.com>
2015-11-16 13:27:57 -0800

    Welcome SVG animation

--- js/logoanimation.js
+++ js/logoanimation.js
@@ -16,3 +16,14 @@ $(".two")
 $(".three")
   .delay(5000)
   .velocity({x: "+=20", y: "100%" });
+
+$(".rect1")
+  .delay(4000)
+  .velocity({ fill: "#fff", strokeWidth: 0})
+  .velocity({x: "+=20", y: "100%" });
+
+  $(".rect2")
+  .delay(2500)
+  .velocity({rotateZ: "360deg"})
+  .delay(500)
+  .velocity({x: "+=20", y: "100%" });
